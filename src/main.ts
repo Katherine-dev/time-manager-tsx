e122b1a58ecb69a7374f6c97d1c96afba68b071d
vorotnikova.e.u <evorotnikowa@yandex.ru>
2022-05-30 12:51:14 +0300

    Feat: add normalize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
-import vuetify from './plugins/vuetify' // path to vuetify export
+import vuetify from './plugins/vuetify'
 import router from './router'
+import 'normalize.css'
 
 Vue.config.productionTip = false
 
